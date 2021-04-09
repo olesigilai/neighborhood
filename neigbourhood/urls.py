02440bf0a78b4876b2fb63296093d075ad1c2050
olesigilai <ole_sigilai@yahoo.com>
2021-04-10 02:11:22 +0300

    urls for django registartion are added to the project urls

--- neigbourhood/urls.py
+++ neigbourhood/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+# from django.conf.urls import include,path,url
+from django.urls import include,path
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include ('neighbor.urls'))
+    path('admin/', admin.site.urls),  
+    
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include ('neighbor.urls')),
+    
+
 ]
