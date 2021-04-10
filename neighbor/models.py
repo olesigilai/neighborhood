de6363bb987920848d2a19b43c264a9a198a0516
olesigilai <ole_sigilai@yahoo.com>
2021-04-11 02:21:52 +0300

    health class model is created bearing contact field,emai,address,neightbohood as a foreign key etc

--- neighbor/models.py
+++ neighbor/models.py
@@ -82,17 +82,17 @@ class healthservices(models.Model):
     def delete_healthservices(cls,healthservices):
         cls.objects.filter(healthservices=healthservices).delete()
 
-# class Health(models.Model):
-#     logo = models.ImageField(upload_to='healthlogo/')
-#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-#     name =models.CharField(max_length=100)
-#     email = models.EmailField()
-#     contact = models.IntegerField()
-#     address =models.CharField(max_length=100)
-#     healthservices = models.ManyToManyField(healthservices)
+class Health(models.Model):
+    logo = models.ImageField(upload_to='healthlogo/')
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    address =models.CharField(max_length=100)
+    healthservices = models.ManyToManyField(healthservices)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 
 # class Authorities(models.Model):
