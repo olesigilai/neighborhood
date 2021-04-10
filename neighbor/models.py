3d59b7bde4b9758750de177268a5daa4813a56f8
olesigilai <ole_sigilai@yahoo.com>
2021-04-10 13:46:06 +0300

    class neighbourhood is created and assigned a characterfiled of max length 100

--- neighbor/models.py
+++ neighbor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
