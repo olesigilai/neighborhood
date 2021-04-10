5120bdf749da97d111d2446de2319ba36b3b9e40
olesigilai <ole_sigilai@yahoo.com>
2021-04-11 02:15:46 +0300

    blogpost class model is created with fields for title,image ,post,usetname and neighbouhood agai a as a foreignKey

--- neighbor/models.py
+++ neighbor/models.py
@@ -33,17 +33,17 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
-# class BlogPost(models.Model):
-#     title = models.CharField(max_length=150)
-#     image = models.ImageField(upload_to='post/')
-#     post = HTMLField()
-#     username = models.ForeignKey(User,on_delete=models.CASCADE)
-#     neighbourhood= models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-#     post_date = models.DateTimeField(auto_now_add=True)
-#     profpic = models.ImageField(upload_to='profpics/')
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='post/')
+    post = HTMLField()
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood= models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+    profpic = models.ImageField(upload_to='profpics/')
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
 # class Comment(models.Model):
 #     comment = models.CharField(max_length=300)
