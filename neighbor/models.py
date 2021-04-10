86ae28a5300e9bf78bcc1a0e3a473c96ce3f41c4
olesigilai <ole_sigilai@yahoo.com>
2021-04-11 02:27:53 +0300

    notification class model is created carrying fields for; title,notification,author,post_date and neohbohoood as a foreign key

--- neighbor/models.py
+++ neighbor/models.py
@@ -106,14 +106,14 @@ class Authorities(models.Model):
         return self.name
     
 
-# class notifications(models.Model):
-#     title = models.CharField(max_length=100)
-#     notification = HTMLField()
-#     priority = models.CharField(max_length=15,choices=Priority,default="Informational")
-#     author = models.ForeignKey(User,on_delete=models.CASCADE)
-#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-#     post_date = models.DateTimeField(auto_now_add=True)
+class notifications(models.Model):
+    title = models.CharField(max_length=100)
+    notification = HTMLField()
+    priority = models.CharField(max_length=15,choices=Priority,default="Informational")
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
