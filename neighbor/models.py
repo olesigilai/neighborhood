7f19574c0ee95fa689324134328502fd0fff70d3
olesigilai <ole_sigilai@yahoo.com>
2021-04-11 02:24:16 +0300

    authorities class model is created with neihgbohood field as a foreign key, email etc

--- neighbor/models.py
+++ neighbor/models.py
@@ -95,15 +95,15 @@ class Health(models.Model):
         return self.name
 
 
-# class Authorities(models.Model):
-#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-#     name =models.CharField(max_length=100)
-#     email = models.EmailField()
-#     contact = models.IntegerField()
-#     address =models.CharField(max_length=100)
+class Authorities(models.Model):
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    address =models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
     
 
 # class notifications(models.Model):
