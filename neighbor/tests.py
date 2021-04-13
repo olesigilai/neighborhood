9e6951f370fcddc55fc27afa5b8d6d3096ce517c
olesigilai <ole_sigilai@yahoo.com>
2021-04-13 10:27:50 +0300

    tests added

--- neighbor/tests.py
+++ neighbor/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import healthservices,neighbourhood
 
+import datetime as dt
 # Create your tests here.
+class neighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.kataret = neighbourhood(neighbourhood='kataret')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kataret,neighbourhood))
+
+    def tearDown(self):
+        neighbourhood.objects.all().delete()
+
+    def test_save_method(self):
+        self.kataret.save_neighbourhood()
+        hood = neighbourhood.objects.all()
+        self.assertTrue(len(hood)>0)
+
